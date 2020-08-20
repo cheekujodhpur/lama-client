ec1d1089a2bf4e58af226bda5b0c277a4a963c77
Kumar Ayush <cheekujodhpur@gmail.com>
2020-08-20 12:03:14 +0530

    react setup

---
+++ src/Alias.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Alias.css';
+
+class Alias extends Component {
+    render() {
+        return (
+            <div className="Alias">
+            Enter Alias
+            </div>
+        );
+    }
+}
+
+export default Alias;
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Alias from './Alias';
+
+class App extends Component {
+    render() {
+        return (
+            <Alias />
+        );
+    }
+}
+
+export default App;
